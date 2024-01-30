f4ebda937f79a15861e7eb9e17edb6e32f6dcc0e
rbarajas28 <rbarajas28@gmail.com>
2024-01-29 21:06:41 -0600

    replaced useDate with UseQuery

--- src/components/GameGrid.tsx
+++ src/components/GameGrid.tsx
@@ -14,7 +14,7 @@ const GameGrid = ({ gameQuery }: Props) => {
   const { data, error, isLoading } = useGames(gameQuery);
   const skeletons = [1, 2, 3, 4, 5, 6];
 
-  if (error) return <Text>{error}</Text>;
+  if (error && error.message) return <Text>{error.message}</Text>;
   return (
     <SimpleGrid
       columns={{ sm: 1, md: 2, lg: 3, xl: 4 }}
@@ -27,7 +27,7 @@ const GameGrid = ({ gameQuery }: Props) => {
             <GameCardSkeleton />
           </CameCardContainer>
         ))}
-      {data.map((game) => (
+      {data?.results.map((game) => (
         <CameCardContainer key={game.id}>
           <GameCard game={game} />
         </CameCardContainer>
--- src/services/api-client.ts
+++ src/services/api-client.ts
@@ -1,8 +1,13 @@
 import axios from "axios";
 
+export interface FetchResponse<T> {
+  count: number;
+  results: T[];
+}
+
 export default axios.create({
-    baseURL: "https://api.rawg.io/api",
-    params: {
-        key: "06e8a48bbf0c483088dd4fc4bac8a73b"
-    }
-})
+  baseURL: "https://api.rawg.io/api",
+  params: {
+    key: "06e8a48bbf0c483088dd4fc4bac8a73b",
+  },
+});
